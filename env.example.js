3fab850eef89b966e7579b2ee8d56e7aae59d36b
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-11-14 14:39:06 +0100

    add example environment configuration for Google Maps API key

---
+++ env.example.js
@@ -0,0 +1,3 @@
+export const GOOGLE_MAPS_API_KEY =
+  "REPLACE_WITH_YOUR_KEY, KEEP QUATATION MARKS";
+// Rename this file to env.js and replace the string above with your actual Google Maps API key.
