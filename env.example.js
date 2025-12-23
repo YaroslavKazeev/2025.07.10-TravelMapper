9999a851e125cf88085bbd15c1fd5ad37b8bc17d
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-12-23 10:48:14 +0100

    refactor: update import path for GOOGLE_MAPS_API_KEY and restore example environment configuration

--- env.example.js
+++
@@ -1,3 +0,0 @@
-export const GOOGLE_MAPS_API_KEY =
-  "REPLACE_WITH_YOUR_KEY, KEEP QUATATION MARKS";
-// Rename this file to env.js and replace the string above with your actual Google Maps API key.
